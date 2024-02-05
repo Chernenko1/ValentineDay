86b85b8134a792bb6fe49c13c0ddb903614f82bb
Chertenko <kchek816@gmail.com>
2024-02-05 14:47:50 +0300

    button text

--- src/components/HomeComponents/Main.tsx
+++ src/components/HomeComponents/Main.tsx
@@ -2,6 +2,25 @@ import { Image, StyleSheet, Text, View } from "react-native"
 import { Button } from "../components/Button"
 import { useState } from "react"
 
+const BUTTON_TEXT: {} = {
+    0: 'No',
+    1: 'Are you sure?',
+    2: 'Really sure?',
+    3: 'Think again',
+    4: 'Last chance!',
+    5: 'Surely not?',
+    6: 'You might regret this!',
+    7: 'Give it another thought!',
+    8: 'Are you absolutely certain?',
+    9: 'This could be a mistake!',
+    10: 'Have a heart!',
+    11: 'Don\'t be so cold!',
+    12: 'Change of heart?',
+    13: 'Wouldn\'t you reconsider',
+    14: 'Is that your final answer?',
+    15: 'You\'re breaking my heart :('
+}
+
 export const Main = () => {
     const [noCount, setNoCount] = useState(0)
     const [yesSize, setYesSize] = useState(18)
@@ -13,7 +32,7 @@ export const Main = () => {
            <Image source={require('../../../assets/jbear.gif')} style={styles.gif}/>
            <View style={styles.buttonView}>
                 <Button text="Yes!" color="green" onPress={() => console.log('yes')} textSize={yesSize}/>
-                <Button text = "No!" color="red" onPress={() => setNoCount(noCount + 1)}/>
+                <Button text = {noCount <= 15 ? BUTTON_TEXT[noCount] : BUTTON_TEXT[15]} color="red" onPress={() => setNoCount(noCount + 1)}/>
            </View>
         </View>
     )
